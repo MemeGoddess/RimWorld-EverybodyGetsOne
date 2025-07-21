9ec029d792f772eb5d46ae87834c34ea34552759
Lana Avery <memekoten@gmail.com>
2025-07-21 12:11:23 +1000

    Post release fixes

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,41 @@
-$scriptPath = ".github/actions/download-steam-workshop/setup.ps1"
+function workshop {
+    if (Get-Command "steamcmd" -ErrorAction SilentlyContinue) {
+        Write-Host "steamcmd is installed."
+    } else {
+        Write-Host "steamcmd is not installed."
+        exit 1
+    }
 
-if (Test-Path $scriptPath) {
-    Write-Host "Running script: $scriptPath"
-    & $scriptPath
-} else {
-    Write-Error "Script not found at path: $scriptPath"
+    $workshopItemsFile = "RequiredWorkshopItems.txt"
+    $workshopItems = ""
+
+    if (Test-Path $workshopItemsFile) {
+        $workshopItems = Get-Content $workshopItemsFile | Where-Object { $_ -ne "" } | ForEach-Object { "+workshop_download_item 294100 $_" }
+        Write-Host "Workshop items: '$workshopItems'"
+    } else {
+        Write-Host "RequiredWorkshopItems.txt file not found. Nothing to download."
+        exit 1
+    }
+
+    # Set the download location to Workshop
+    $scriptDirectory = $PSScriptRoot
+    $downloadPath = Join-Path $scriptDirectory "Workshop"
+    if (-not (Test-Path $downloadPath)) {
+        New-Item -ItemType Directory -Path $downloadPath | Out-Null
+    }
+
+    # Download workshop items for appid 294100
+    &steamcmd +force_install_dir $downloadPath +login anonymous $workshopItems +quit
+
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Failed to download workshop item ID: $itemId"
+        exit 1
+    }
+
+    Write-Host "Workshop items downloaded successfully."
+
 }
+
+# Call the function
+workshop
+Write-Host "Setup complete. Solution should be buildable now."
